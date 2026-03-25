b8bdef3eb20817dd76379ebb82ac12f0f5741a1c
Mason Perdue <220426478+masonperdue@users.noreply.github.com>
2026-03-25 10:46:37 -0700

    Only monitor batteries and solar

--- solar_regatta.ino
+++ solar_regatta.ino
@@ -8,6 +8,16 @@ Adafruit_ADS1115 adc; // ADDR to GND
 
 // Define variables
 float batCapRemaining = batCapacity;  // As
+long adc_ain0_div = 0;
+long adc_ain1_bat = 0;
+long adc_ain2_sol = 0;
+float volts_div = 0.0;
+float volts_bat = 0.0;
+float volts_sol = 0.0;
+float amps_div = 0.0;
+float amps_bat = 0.0;
+float amps_sol = 0.0;
+float batSoC = 0.0;
 
 void setup(void)
 {
@@ -27,62 +37,9 @@ void setup(void)
 
 void loop(void)
 {
-  // Define variables
-  long adc_ain0_div = 0;
-  long adc_ain1_mot = 0;
-  long adc_ain2_bat = 0;
-  long adc_ain3_sol = 0;
-  float volts_div = 0.0;
-  float volts_mot = 0.0;
-  float volts_bat = 0.0;
-  float volts_sol = 0.0;
-  float amps_div = 0.0;
-  float amps_mot = 0.0;
-  float amps_bat = 0.0;
-  float amps_sol = 0.0;
-  float batSoC = 0.0;
+  monitorCurrent();
 
-  // Read each analog input pin single-ended (10 times in a second)
-  for (int i = 0; i < 10; i++)
-  {
-    adc_ain0_div += adc.readADC_SingleEnded(0);
-    adc_ain1_mot += adc.readADC_SingleEnded(1);
-    adc_ain2_bat += adc.readADC_SingleEnded(2);
-    adc_ain3_sol += adc.readADC_SingleEnded(3);
-
-    // Wait 1/10s
-    delay(100);
-  }
-  
-  // Average 10 readings in a second
-  adc_ain0_div /= 10;
-  adc_ain1_mot /= 10;
-  adc_ain2_bat /= 10;
-  adc_ain3_sol /= 10;
-
-  // Convert analog read to volts
-  volts_div = adc.computeVolts(adc_ain0_div);
-  volts_mot = adc.computeVolts(adc_ain1_mot);
-  volts_bat = adc.computeVolts(adc_ain2_bat);
-  volts_sol = adc.computeVolts(adc_ain3_sol);
-
-  // Convert voltage to current
-  amps_mot = voltsToAmps(volts_mot, volts_div);
-  amps_bat = voltsToAmps(volts_bat, volts_div);
-  amps_sol = voltsToAmps(volts_sol, volts_div);
-
-  // Track battery SoC
-  if (amps_bat > -0.2 && amps_bat < 0.2)
-  {
-    batCapRemaining -= amps_bat * 1;  // Since loop runs every second, time is 1s
-    batSoC = batCapRemaining / batCapacity * 100.0; // Percentage
-  }
-
-  // Print analog inputs and calculated voltages to serial
-  Serial.println("-----------------------------------------------------------");
-  Serial.print("Battery: "); /* Serial.print(adc_ain2_bat); Serial.print("  "); Serial.print(volts_bat); Serial.print("V\t"); */ Serial.print(amps_bat); Serial.print("A\tSoC: "); Serial.print(batSoC); Serial.println("%");
-  Serial.print("Solar: "); /* Serial.print(adc_ain2_sol); Serial.print("  "); Serial.print(volts_sol); Serial.print("V\t"); */ Serial.print(amps_sol); Serial.print("A");
-  Serial.print("Motor: "); /* Serial.print(adc_ain1_mot); Serial.print("  "); Serial.print(volts_mot); Serial.print("V\t"); */ Serial.print(amps_mot); Serial.println("A");
+  // monitorBatteries();
 }
 
 float voltsToAmps(float currentVolts, float zeroVolts)
@@ -90,9 +47,50 @@ float voltsToAmps(float currentVolts, float zeroVolts)
   return ((currentVolts - zeroVolts) * 1000) / 40;  // difference in measured and resting voltage times 1000 to get mV and divided by 40 mV/A
 }
 
+void monitorCurrent()
+{
+  // Read each analog input pin single-ended (10 times in a second)
+  for (int i = 0; i < 10; i++)
+  {
+    adc_ain0_div += adc.readADC_SingleEnded(0);
+    adc_ain1_bat += adc.readADC_SingleEnded(1);
+    adc_ain2_sol += adc.readADC_SingleEnded(2);
 
+    // Wait 1/10s
+    delay(100);
+  }
+  
+  // Average 10 readings in a second
+  adc_ain0_div /= 10;
+  adc_ain1_bat /= 10;
+  adc_ain2_sol /= 10;
 
+  // Convert analog read to volts
+  volts_div = adc.computeVolts(adc_ain0_div);
+  volts_bat = adc.computeVolts(adc_ain1_bat);
+  volts_sol = adc.computeVolts(adc_ain2_sol);
 
+  // Convert voltage to current
+  amps_bat = voltsToAmps(volts_bat, volts_div);
+  amps_sol = voltsToAmps(volts_sol, volts_div);
+
+  // Track battery SoC
+  if (amps_bat < -0.2 || amps_bat > 0.2)
+  {
+    batCapRemaining -= amps_bat * 1;  // Since loop runs every second, time is 1s
+    batSoC = batCapRemaining / batCapacity * 100.0; // Percentage
+  }
+
+  // Print analog inputs and calculated voltages to serial
+  Serial.println("---------");
+  Serial.print("Battery: "); Serial.print(amps_bat); Serial.print("A\tSoC: "); Serial.print(batSoC); Serial.println("%");
+  Serial.print("Solar: "); Serial.print(amps_sol); Serial.println("A");
+}
+
+// void monitorBatteries()
+// {
+
+// }
 
 
 
